59f3a0bd2d84e047e7aeaf18729edccaa2b838f2
Siewert Lameijer <Siewert308SW@users.noreply.github.com>
2024-09-17 01:05:11 +0200

    Add files via upload

---
+++ ecoflow_baseload.php
@@ -0,0 +1,284 @@
+<?php
+
+//															       //
+// ****************************************************************//
+//          EcoFlow micro-inverter automatic baseload              //
+// ****************************************************************//
+//                                                                 //
+
+// Debug?
+	$debug				  = 'yes';			    				   // Waarde 'yes' of 'no'.
+
+// Tijd variables
+	$invStartTime 		  = '00:00';							   // Tijd dat de omvormer mag starten met terugleveren
+	$invEndTime  		  = '14:00'; 							   // Tijd dat de omvormer moet stoppen met terugleveren
+
+// Omvormer variables
+	$ecoflowMaxOutput 	  = 600; 				    			   // Maximale aantal output Watts wat de omvormer kan leveren. 
+	$gridLoadMargin		  = 5;									   // Trek deze value (watts) af van de nieuwe baseload, Deze value wordt alsnog van het net wordt getrokken om teruglevering te voorkomen
+	$minBatteryVoltage 	  = 23.0; 								   // Minimale Voltage wat in de accu moet blijven
+
+// Homewizard variables
+	$hwP1IP 			  = 'homewizardP1IP'; 					   // IP Homewizard P1 Meter
+	$hwKwhIP 			  = 'homewizardKwhMeter';     			   // IP Homewizard Solar kwh Meter
+
+// Domoticz variables
+	$domoticzIP 		  = 'domoticzIP:port'; 			    	   // IP + poort van Domoticz
+	$chargerIDX 		  = 'idx'; 							       // On/Off switch lader in Domoticz
+	$chargerUsageIDX 	  = 'idx'; 							       // Verbruik lader in Domoticz
+
+// Ecoflow Powerstream API variables
+	$ecoflowPath 		  = '/path/to/files/';                     // Path waar je scripts zich bevinden
+	$ecoflowAccessKey	  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';	   // Powerstream API access key
+	$ecoflowSecretKey	  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';	   // Powerstream API secret key
+	$ecoflowSerialNumber  = ['HWXXXXXXXXXXXXXX',];				   // Powerstream serie nummer
+
+//															       //
+// ****************************************************************//
+//             EcoFlow micro-inverter start script                 //
+// ****************************************************************//
+//                                                                 //
+
+// Path naar ecoflow API class file
+	include(''.$ecoflowPath.'ecoflow-api-class.php');
+
+// php.ini slikt de timezone niet
+	date_default_timezone_set('Europe/Amsterdam');
+
+// Tijd nu
+	$timeNow = date('H:i');
+
+// Get EcoFlow Status
+	$ecoflow = new EcoFlowAPI(''.$ecoflowAccessKey.'', ''.$ecoflowSecretKey.'');
+	$serial_number = file_get_contents(''.$ecoflowPath.'serialnumber.txt');	
+	if ($serial_number === false) {
+		if ($debug == 'yes'){
+		echo ' -- ERROR: Kan het serialnumber.txt bestand niet openen!'.PHP_EOL;
+		echo ' '.PHP_EOL;
+		echo ' ----------------------------------'.PHP_EOL;
+		}
+	exit(1);
+	}
+
+	if (empty(trim($serial_number))) {
+		if ($debug == 'yes'){
+		echo ' -- ERROR: Accus is leeg!'.PHP_EOL;
+		echo ' '.PHP_EOL;
+		echo ' ----------------------------------'.PHP_EOL;
+		}
+		foreach ($ecoflowSerialNumber as $sn) {
+			$ecoflow->setDeviceFunction($sn, 'WN511_SET_PERMANENT_WATTS_PACK', ['permanent_watts' => 0]);
+		}
+	exit(0);
+	} else {
+		$inv = $ecoflow->getDevice($serial_number);
+		if (!$inv || !isset($inv['data']['20_1.permanentWatts'])) {
+			if ($debug == 'yes'){
+			echo ' -- ERROR: Kan gegevens van de omvormer niet ophalen!'.PHP_EOL;
+			echo ' '.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			}
+	exit(1);
+	}
+		
+// Get Baseload
+		$currentBaseload	  = ($inv['data']['20_1.permanentWatts']) / 10;
+	
+// Domoticz URLs
+		$DomoticzJsonUrl 	  = 'http://'.$domoticzIP.'/json.htm?type=command&param=getdevices&rid=';
+		$DomoticzJsonResult   = ['charger' => $DomoticzJsonUrl . $chargerIDX,'chargerUsage' => $DomoticzJsonUrl . $chargerUsageIDX];
+
+// Get Lader status
+		$charger 			  = json_decode(file_get_contents($DomoticzJsonResult['charger']), true)['result'][0]['Status'] ?? 'Off';
+	
+// Get huidige Lader verbruik in Domoticz
+		$chargerUsage_data    = json_decode(file_get_contents($DomoticzJsonResult['chargerUsage']), true);
+		$chargerUsage 		  = intval($chargerUsage_data['result'][0]['Data'] ?? 0);
+		
+// Get batterij Voltage in Domoticz
+		$pv1InputVolt 		  = $inv['data']['20_1.pv1InputVolt'];
+		$pv2InputVolt 		  = $inv['data']['20_1.pv2InputVolt'];
+		$pvAvInputVoltAverage = ($pv1InputVolt + $pv2InputVolt) / 2;
+		$pvAvInputVoltRounded = ($pvAvInputVoltAverage) / 10;
+		$pvAvInputVoltage     = $pvAvInputVoltRounded;
+
+// Batterij leeg?	
+		if ($pvAvInputVoltage <= $minBatteryVoltage) {
+		$batteryEmpty = 1;
+		} else {
+		$batteryEmpty = 0;
+		}
+
+// Get EcoFlow input Watts
+		$pv1InputWatts 		  = $inv['data']['20_1.pv1InputWatts'];
+		$pv2InputWatts 		  = $inv['data']['20_1.pv2InputWatts'];
+		$pvAvInputWattsAverage= ($pv1InputWatts + $pv2InputWatts);
+		$pvAvInputWattsRounded= ($pvAvInputWattsAverage) / 10;
+		$pvAvInputWatts       = $pvAvInputWattsRounded;
+	
+// Get HomeWizard P1 verbruik
+		$hwP1 = curl_init();
+		curl_setopt($hwP1, CURLOPT_URL, "http://".$hwP1IP."/api/v1/data");
+		curl_setopt($hwP1, CURLOPT_RETURNTRANSFER, true);
+		$hwP1result = curl_exec($hwP1);
+
+		if (curl_errno($hwP1)) { echo curl_error($hwP1); }
+		else {
+		  $hwP1UsageDecode = json_decode($hwP1result);
+		  $hwP1UsageProp = round($hwP1UsageDecode->active_power_w);
+		  $hwP1Usage = round($hwP1UsageProp);
+		}
+
+// Get HomeWizard PV (kwh meter) opwek
+		$hwSolar = curl_init();
+		curl_setopt($hwSolar, CURLOPT_URL, "http://".$hwKwhIP."/api/v1/data");
+		curl_setopt($hwSolar, CURLOPT_RETURNTRANSFER, true);
+		$hwSolarresult = curl_exec($hwSolar);
+
+		if (curl_errno($hwSolar)) { echo curl_error($hwSolar); }
+		else {
+		  $hwSolarProductionDecode = json_decode($hwSolarresult);
+		  $hwSolarProduction = $hwSolarProductionDecode->active_power_w;
+		  $PVProduction = round($hwSolarProduction);
+		}
+		
+// Get HomeWizard Socket opwek
+		$hwSocket = curl_init();
+		curl_setopt($hwSocket, CURLOPT_URL, "http://".$hwSocketIP."/api/v1/data");
+		curl_setopt($hwSocket, CURLOPT_RETURNTRANSFER, true);
+		$hwSocketresult = curl_exec($hwSocket);
+
+		if (curl_errno($hwSocket)) { echo curl_error($hwSocket); }
+		else {
+		  $hwSocketProductionDecode = json_decode($hwSolarresult);
+		  $hwSocketProduction = $hwSolarProductionDecode->active_power_w;
+		  $SocketProduction = round($hwSocketProduction);
+		}
+			
+		if (json_last_error() === JSON_ERROR_NONE) {
+		
+// Schakeltijd			
+		if (date('H:i') >= ( ''.$invStartTime.'' ) && date('H:i') <= ( ''.$invEndTime.'' )) {
+			$schedule = 1;
+		} else {
+			$schedule = 0;
+		}
+
+// Bepaal de nieuwe baseload
+		$realUsage = ($hwP1Usage - $PVProduction) + $currentBaseload;
+		
+		if ($hwP1Usage >= $ecoflowMaxOutput && $PVProduction == 0) {
+			$newBaseload = $ecoflowMaxOutput;
+		} elseif ($hwP1Usage < $ecoflowMaxOutput && $PVProduction == 0) {
+			$newBaseload = ($realUsage) - $gridLoadMargin;
+		} else {
+			$newBaseload = 0;
+		}
+
+		$baseload = ($newBaseload) * 10;		
+
+// Vermogen op 0 zetten indien oplader actief is
+		if ($charger == 'On' && $chargerUsage > 0) {
+			$baseload = 0;
+		}
+	
+// Vermogen op 0 zetten als de batterij leeg is
+		if ($pvAvInputVoltage <= $minBatteryVoltage) {
+			$baseload = 0;
+		}
+	
+// Vermogen op 0 zetten indien schakeltijd negatief is
+		if ($schedule == 0) {
+			$baseload = 0;
+		}
+
+// Print Begin
+		if ($debug == 'yes'){
+			echo ' '.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			echo ' --  Powerstream micro-inverter  --'.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			echo ' '.PHP_EOL;
+		}
+	
+// Print Schakeltijd
+			if ($debug == 'yes'){
+			echo '-/- Schakeltijd       -\-'.PHP_EOL;
+			echo ' -- Start Tijd         : '.$invStartTime.''.PHP_EOL;
+			echo ' -- Eind Tijd          : '.$invEndTime.''.PHP_EOL;
+			}
+			
+		if (date('H:i') > ( ''.$invStartTime.'' ) && date('H:i') <= ( ''.$invEndTime.'' )) {
+			if ($debug == 'yes'){
+			echo ' -- Schakeltijd        : true'.PHP_EOL;
+			echo ' '.PHP_EOL;
+			}
+		} else {
+			if ($debug == 'yes'){
+			echo ' -- Schakeltijd        : false'.PHP_EOL;
+			echo ' '.PHP_EOL;
+			}
+		}
+	
+// Print Lader Status
+		if ($debug == 'yes'){
+			echo '-/- Lader    -\-'.PHP_EOL;
+			echo ' -- Lader              : '.$charger.''.PHP_EOL;
+			echo ' -- Lader Verbruik     : '.$chargerUsage.'w'.PHP_EOL;
+			echo ' '.PHP_EOL;
+
+// Print Batterij Status	
+			echo '-/- Batterij    -\-'.PHP_EOL;
+			echo ' -- Batterij Voltage   : '.$pvAvInputVoltage.'v'.PHP_EOL;
+			if ($batteryEmpty == 1) {
+			echo ' -- Batterij           : leeg'.PHP_EOL;
+			} else {
+			echo ' -- Batterij           : vol'.PHP_EOL;
+			}
+			echo ' '.PHP_EOL;
+		}
+	
+// Print Energie Status
+		if ($debug == 'yes'){
+			echo '-/- Energie           -\-'.PHP_EOL;
+			echo ' -- P1 Verbruik        : '.$hwP1Usage.'w'.PHP_EOL;
+			echo ' -- PV Opwek           : '.$PVProduction.'w'.PHP_EOL;
+			echo ' -- Echte verbruik     : '.$realUsage.'w'.PHP_EOL;
+			if ($hwP1Usage > 100) {
+			echo ' -- Stroom vraag       : true'.PHP_EOL;
+			} else {
+			echo ' -- Stroom vraag       : false'.PHP_EOL;
+			}
+			echo ' '.PHP_EOL;
+		}
+	
+// Print Nieuwe Baseload
+		if ($debug == 'yes'){
+			echo '-/- EcoFlow Inverter  -\-'.PHP_EOL;
+			echo ' -- Huidige Baseload   : '.$currentBaseload.'w'.PHP_EOL;
+			echo ' -- Nieuwe Baseload    : '.$newBaseload.'w'.PHP_EOL;	
+		}
+	
+// Update baseload
+		if ($baseload != $currentBaseload) {
+			if ($debug == 'yes'){
+			echo ' -- Baseload update    : true'.PHP_EOL;
+			}	
+			$ecoflow->setDeviceFunction($serial_number, 'WN511_SET_PERMANENT_WATTS_PACK', ['permanent_watts' => $baseload]);
+		} else {
+			if ($debug == 'yes'){
+			echo ' -- Baseload update    : false'.PHP_EOL;	
+			}
+		}
+	
+// Print Einde
+		if ($debug == 'yes'){
+			echo ' '.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			echo ' --           The End            --'.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			echo ' '.PHP_EOL;
+		}
+	}
+}
+?>
---
+++ ecoflow_charging.php
@@ -0,0 +1,274 @@
+<?php
+//															 //
+// **********************************************************//
+//     PowerQueen LiFepo4 20A Charger automatic charging     //
+// **********************************************************//
+//                                                           //
+
+// Debug?
+$debug					= 'yes';								 // Waarde 'yes' of 'no'.
+
+// Homewizard variables
+$hwP1IP 				= 'homewizardP1IP'; 			     	 // IP Homewizard P1 Meter
+
+// Domoticz variables
+$domoticzIP			    = 'domoticzIP:port'; 	    		     // IP + poort van Domoticz
+$chargerIDX 			= 'idx'; 								 // On/Off switch lader in Domoticz
+$chargerWattsIDX 		= 'idx'; 								 // watts-meter tbv oplader in Domoticz
+$quookerWattsIDX 		= 'idx'; 								 // watts-meter tbv Quooker in Domoticz
+$counter_1WattsIDX 		= 'idx'; 								 // watts-meter tbv KoffieAutomaat in Domoticz
+$counter_2WattsIDX 		= 'idx';
+$bedroomWattsIDX 		= 'idx'; 								 // watts-meter tbv slaapkamer in Domoticz
+$voltageIDX 			= 'idx'; 								 // Voltage-meter in Domoticz
+
+// Lader/Batterij variables
+$minBatteryVoltage 		= 23.0; 								 // Minimale Voltage wat in de accu moet blijven.
+$maxPowerReturn			= -600;									 // Minimale Wattage teruglevering wanneer de lader moet starten
+$maxPowerUsage			= 250;									 // Maximale Wattage verbruik wanneer de lader moet stoppen
+
+// Ecoflow Powerstream API variables
+$ecoflowPath 			= '/path/to/files/';                     // Path waar de scripts zich bevinden
+$ecoflowAccessKey		= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';	 // Powerstream API access key
+$ecoflowSecretKey		= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';	 // Powerstream API secret key
+$ecoflowSerialNumber 	= ['HWXXXXXXXXXXXXXX',];				 // Powerstream serie nummer
+
+//															 //
+// **********************************************************//
+//       PowerQueen LiFepo4 20A charging start script        //
+// **********************************************************//
+//                                                           //
+
+
+	if ($debug == 'yes'){
+	echo ' '.PHP_EOL;
+	echo ' ----------------------------------'.PHP_EOL;
+	echo ' --   PowerQueen auto charging   --'.PHP_EOL;
+	echo ' ----------------------------------'.PHP_EOL;
+	echo ' '.PHP_EOL;
+	}
+	
+	require_once(''.$ecoflowPath.'ecoflow-api-class.php');
+
+// Get Ecoflow status
+	$ecoflow = new EcoFlowAPI(''.$ecoflowAccessKey.'', ''.$ecoflowSecretKey.'');
+	$ecoflowSerialNumber = file_get_contents(''.$ecoflowPath.'serialnumber.txt');
+		$batterijEmpty = 0;
+	if ($ecoflowSerialNumber === false) {
+		if ($debug == 'yes'){
+		echo ' -- ERROR: Kan het serialnumber.txt bestand niet openen!'.PHP_EOL;
+		echo ' '.PHP_EOL;
+		echo ' ----------------------------------'.PHP_EOL;
+		}
+		exit(1);
+	}
+
+	if (empty(trim($ecoflowSerialNumber))) {
+		if ($debug == 'yes'){
+		echo ' -- ERROR: Accus is leeg!'.PHP_EOL;
+		echo ' '.PHP_EOL;
+		echo ' ----------------------------------'.PHP_EOL;
+		}
+			$batterijEmpty = 1;
+	} else {
+		
+		$inv = $ecoflow->getDevice($ecoflowSerialNumber);
+		if (!$inv || !isset($inv['data']['20_1.permanentWatts'])) {
+			if ($debug == 'yes'){
+			echo ' -- ERROR: Kan gegevens van de omvormer niet ophalen!'.PHP_EOL;
+			echo ' '.PHP_EOL;
+			echo ' ----------------------------------'.PHP_EOL;
+			}
+			exit(1);
+		}
+
+
+// Function switch device
+	function SwitchDevice($idx,$cmd) {
+	  global $domoticzIP;
+	  $reply=json_decode(file_get_contents('http://'.$domoticzIP.'/json.htm?type=command&param=switchlight&idx='.$idx.'&switchcmd='.$cmd),true);
+	  if($reply['status']=='OK') $reply='OK';else $reply='ERROR';
+	  return $reply;
+	}
+
+// Function Update Domoticz device
+	function UpdateDevice($idx,$cmd) {
+	  global $domoticzIP;
+	  $reply=json_decode(file_get_contents('http://'.$domoticzIP.'/json.htm?type=command&param=udevice&idx='.$idx.'&nvalue=0&svalue='.$cmd),true);
+	  if($reply['status']=='OK') $reply='OK';else $reply='ERROR';
+	  return $reply;
+	}
+	
+// URLs
+	$baseUrl = 'http://'.$domoticzIP.'/json.htm?type=command&param=getdevices&rid=';
+	$urls = [
+		'charger'  		     => $baseUrl . $chargerIDX,
+		'chargerWatts'       => $baseUrl . $chargerWattsIDX,
+		'quookerWatts'       => $baseUrl . $quookerWattsIDX,
+		'counter_1Watts'     => $baseUrl . $counter_1WattsIDX,
+		'counter_2Watts'     => $baseUrl . $counter_2WattsIDX,
+		'bedroomWatts'       => $baseUrl . $bedroomWattsIDX,
+		'voltage'  		     => $baseUrl . $voltageIDX
+	];
+	
+// Get Lader status
+	$charger = json_decode(file_get_contents($urls['charger']), true)['result'][0]['Status'] ?? 'ERROR';
+	if ($charger == 'ERROR'){
+		if ($debug == 'yes'){
+		echo ' -- ERROR: Kan gegevens van Domoticz niet ophalen!'.PHP_EOL;
+		echo ' '.PHP_EOL;
+		echo ' ----------------------------------'.PHP_EOL;
+		}
+		exit(1);
+	}
+	
+// Get batterij Voltage
+	$pv1InputVolt 		  = $inv['data']['20_1.pv1InputVolt'];
+	$pv2InputVolt 		  = $inv['data']['20_1.pv2InputVolt'];
+	$pvAvInputVoltAverage = ($pv1InputVolt + $pv2InputVolt) / 2;
+	$pvAvInputVoltRounded = ($pvAvInputVoltAverage) / 10;
+	$pvAvInputVoltage     = $pvAvInputVoltRounded;
+
+
+// Batterij leeg?	
+	if ($pvAvInputVoltage <= $minBatteryVoltage) {
+	$batterijEmpty = 1;
+	} else {
+	$batterijEmpty = 0;
+	}
+
+// Get huidige Domoticz voltage dummy
+	$voltage_data 	      = json_decode(file_get_contents($urls['voltage']), true);
+	$voltage 			  = $voltage_data['result'][0]['Voltage'];
+
+// Update Domoticz voltage device
+	if ($voltage != $pvAvInputVoltage) {
+		UpdateDevice($voltageIDX, ''.$pvAvInputVoltage.'');
+	}
+	
+// Get PV input Watts
+	$pv1InputWatts 		  = ($inv['data']['20_1.pv1InputWatts']) / 10;
+	$pv2InputWatts 		  = ($inv['data']['20_1.pv2InputWatts']) / 10;
+	$pvInputTotalWatts    = ($pv1InputWatts + $pv2InputWatts);
+
+// Get P1 verbruik en teruglevering
+	$hwP1 = curl_init();
+	curl_setopt($hwP1, CURLOPT_URL, "http://".$hwP1IP."/api/v1/data");
+	curl_setopt($hwP1, CURLOPT_RETURNTRANSFER, true);
+	$hwP1result = curl_exec($hwP1);
+
+	if (curl_errno($hwP1)) { echo curl_error($hwP1); }
+	else {
+	  $hwP1UsageDecode = json_decode($hwP1result);
+	  $hwP1UsageProp = round($hwP1UsageDecode->active_power_w);
+	  $P1Usage = round($hwP1UsageProp);
+	}
+	
+// Get huidige Lader verbruik in Domoticz
+	$chargerWatts_data    = json_decode(file_get_contents($urls['chargerWatts']), true);
+	$chargerWatts 		  = intval($chargerWatts_data['result'][0]['Data'] ?? 0);
+
+// Get Huidige Baseload
+	$currentBaseload = ($inv['data']['20_1.permanentWatts']) / 10;
+
+// Bepaal verbruik grootverbruiker
+	$quookerWatts_data    = json_decode(file_get_contents($urls['quookerWatts']), true);
+	$quookerWatts 		  = intval($quookerWatts_data['result'][0]['Data'] ?? 0);
+
+	$counter_1Watts_data  = json_decode(file_get_contents($urls['counter_1Watts']), true);
+	$counter_1Watts 	  = intval($counter_1Watts_data['result'][0]['Data'] ?? 0);
+
+	$counter_2Watts_data  = json_decode(file_get_contents($urls['counter_2Watts']), true);
+	$counter_2Watts 	  = intval($counter_2Watts_data['result'][0]['Data'] ?? 0);
+	
+	$bedroomWatts_data 	  = json_decode(file_get_contents($urls['bedroomWatts']), true);
+	$bedroomWatts 	  	  = intval($bedroomWatts_data['result'][0]['Data'] ?? 0);
+
+// Bepaal powerBreach
+	if ($P1Usage >= $maxPowerUsage && $quookerWatts <= 2 && $counter_1Watts <= 20 && $counter_2Watts <= 20 && $bedroomWatts <= 1000) {
+		$powerBreach = 1;
+	} elseif ($P1Usage >= $maxPowerUsage || $quookerWatts > 2 || $counter_1Watts > 20 || $counter_2Watts > 20 || $bedroomWatts > 1000) {
+		$powerBreach = 0;
+	} elseif ($P1Usage < $maxPowerUsage) {
+		$powerBreach = 0;
+	}
+
+////////////////////////////////////////////////////////////////////////
+
+
+// Print 
+	if ($debug == 'yes'){
+	echo '-/- Batterij                 -\-'.PHP_EOL;
+	echo ' -- Batterij Voltage          : '.$pvAvInputVoltage.'v'.PHP_EOL;
+	if ($batterijEmpty == 1) {
+	echo ' -- Batterij leeg!'.PHP_EOL;
+	}
+	echo ' '.PHP_EOL;
+	echo '-/- Power Breach             -\-'.PHP_EOL;
+	echo ' -- Breached                  : '.$powerBreach.''.PHP_EOL;
+	echo ' '.PHP_EOL;
+	echo '-/- EcoFlow Inverter         -\-'.PHP_EOL;
+	echo ' -- Baseload                  : '.$currentBaseload.'w'.PHP_EOL;
+	echo ' -- Output                    : '.$pvInputTotalWatts.'w'.PHP_EOL;
+	echo ' '.PHP_EOL;
+	echo '-/- P1 Meter                 -\-'.PHP_EOL;
+	if ($P1Usage >= 0){
+	echo ' -- Verbruik                  : '.$P1Usage.'w'.PHP_EOL;
+	} else {
+	echo ' -- Teruglevering             : '.$P1Usage.'w'.PHP_EOL;		
+	}
+	echo ' '.PHP_EOL;
+	echo '-/- Lader                    -\-'.PHP_EOL;
+	echo ' -- Lader Status              : '.$charger.''.PHP_EOL;
+	}
+	
+//
+// **********************************************************
+// Control PowerQueen LiFepo4 20A automatic charging
+// **********************************************************
+//
+
+// Lader AAN bij genoeg zonnestroom en Batt% onder x.x%
+	if ($charger == 'Off' && $P1Usage <= $maxPowerReturn && $currentBaseload == 0 && $pvAvInputVoltage <= 25.7 && $pvInputTotalWatts == 0 && $powerBreach == 0) {
+		if ($debug == 'yes'){
+		echo ' -- Lader ingeschakeld'.PHP_EOL;
+		}
+		switchDevice($chargerIDX, 'On');
+
+// Lader UIT wanneer er niet genoeg stroom word opgewekt door de PV en lader is aan het laden
+	} elseif ($charger == 'On' && $P1Usage >= $maxPowerUsage && $powerBreach == 1) {
+		if ($debug == 'yes'){
+		echo ' -- Lader uitgeschakeld, geen zonnestroom genoeg!'.PHP_EOL;
+		}
+		switchDevice($chargerIDX, 'Off');
+		
+// Lader UIT wanneer batterij vol is
+	//} elseif ($charger == 'On' && $pvAvInputVoltage >= 26.65 && $chargerWatts <= 5.6) {
+	//	if ($debug == 'yes'){
+	//	echo ' -- Lader uitgeschakeld, Batterij vol!'.PHP_EOL;
+	//	}
+	//	switchDevice($chargerIDX, 'Off');
+
+	} else {
+		
+		if ($charger == 'On') {
+		if ($debug == 'yes'){		
+			echo ' -- Batterij wordt opgeladen'.PHP_EOL;
+		}	
+
+		} elseif ($charger == 'Off') {
+			if ($debug == 'yes'){
+			echo ' -- Batterij wordt niet opgeladen'.PHP_EOL;
+			}
+		}
+	}
+
+// Print
+	if ($debug == 'yes'){
+	echo ' '.PHP_EOL;
+	echo ' ----------------------------------'.PHP_EOL;
+	echo ' --           The End            --'.PHP_EOL;
+	echo ' ----------------------------------'.PHP_EOL;
+	echo ' '.PHP_EOL;
+	}
+	}
+?>
