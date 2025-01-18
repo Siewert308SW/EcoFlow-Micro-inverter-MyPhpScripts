ef85b69e0507551cefd5482e2b6076566763dbfd
Siewert <s.j.lameijer@gmail.com>
2025-01-18 17:21:11 +0100

    Update ecoflow_config.php

--- Tryout-2025/config/ecoflow_config.php
+++ Tryout-2025/config/ecoflow_config.php
@@ -9,17 +9,14 @@
 // Debug?
 	$debug				    = 'yes';							 // Waarde 'yes' of 'no'
 
-// Debug?
-	$override				= 'no';							 // Waarde 'yes' of 'no' Bij 'yes' kun je tijdelijk de laders aan laten ongeacht op overschot of niet
+// Manual override opladen
+	$manualChargeOverride	= 'no';							     // Waarde 'yes' of 'no' Bij 'yes' kun je tijdelijk de laders aan laten ongeacht overschot of niet, laders gaan uit wanneer batterijen vol zijn
 	
 // Tijd variables
 	$invStartTime		    = '00:00';							 // Omvormer starttijd (bij $runInfinity == 'no')
 	$invEndTime			    = '12:30';							 // Omvormer eindtijd (bij $runInfinity == 'no')
 	$runInfinity		    = 'yes';		    				 // Waarde 'yes', 'no' bij 'yes' zal de omvormer indien mogelijk en afhankelijk van de instellingen altijd blijven opwekken
-	$runInfinityMidday	    = 'yes';						     // Waarde 'yes', 'no' bij 'yes' zal opladen voorrang hebben in de winter
-	$runInfinityEvening	    = 'no';		    					 // Waarde 'yes', 'no' bij 'yes' zal er in de winter in de avond geen opwek zijn
-	$runInfinityNight	    = 'yes';							 // Waarde 'yes', 'no' bij 'yes' zal er in de winter in de nacht geen opwek zijn
-
+	
 // Lokatie variables tbv $sunset/$sunrise
 	$latitude               = '00.00000';						 // Latitude is de afstand in graden 'Noord' of 'Zuid' tot de evenaar
 	$longitude              = '-0.00000';						 // Longitude is de afstand in graden 'Oost' of 'West' tot de Meridiaan in Greenwich
@@ -31,14 +28,14 @@
 	$ecoflowMaxOutput	    = 600;								 // Maximale teruglevering (Watts) wat de omvormer kan/mag leveren. 
 	$ecoflowMinOutput	    = 100;								 // Minimale teruglevering (Watts) Onder dit getal (Watt) zal de omvormer niet terugleveren. 
 	$ecoflowOutputOffSet    = 10;								 // Trek deze value (watts) af van de nieuwe baseload, Deze value wordt alsnog van het net wordt getrokken om teruglevering te voorkomen
-	$maxInvTemp             = 65;								 // Maximale interne temperatuur, daarboven stopt de omvormer met terugleveren 
+	$maxInvTemp             = 67;								 // Maximale interne temperatuur, daarboven stopt de omvormer met terugleveren 
 
 // Batterij variables
 	$batteryVolt		    = 25.6;								 // Voltage van de batterij
 	$batteryAh              = 200;                               // Totale Ah van alle batterijen
-	$chargerEfficiency      =  78.6;                             // Lader laad efficientie
-	$batteryMinimum		    =  5;                               // Minimale procenten die in de batterij moeten blijven
-	$batterySaveUp			= 'yes';							 // Waarde 'yes' of 'no' Bij 'yes' zal de omvormer niet eerder teruglveren als je ladd cycles 100% is.
+	$chargerEfficiency      = 83.4;                              // Lader laad efficientie
+	$batteryMinimum		    = 10;                                 // Minimale procenten die in de batterij moeten blijven
+	$batterySaveUp			= 'yes';							 // Waarde 'yes' of 'no' Bij 'yes' zal de omvormer niet eerder teruglveren als je laad cycles 100% is.
 
 // Homewizard variables
 	$hwP1IP				    = '000.000.000.00';					 // IP Homewizard P1 Meter
@@ -62,6 +59,17 @@
 // Battery BMS variables
 	$keepBMSalive		    = 'yes';                             // Indien batterij is leeg getrokken zal er een beetje bij geladen worden om de BMS wakker te houden
 	$bmsMinimumVoltage      = 22.6;                              // Minimale Voltage van de batterij (volgens de EcoFlow app), De batterij zal een beetje bijladen om de BMS wakker te houden
+
+// Domoticz variables
+	$domoticzIP			    = '127.0.0.1:8080'; 	    	 // IP + poort van Domoticz
+	$heaterWcdIDX           = '';
+	$quookerWcdIDX			= '';
+	$aanrecht1WcdIDX		= '';
+	$aanrecht2WcdIDX	    = '';
+	$natalyaWcdIDX	        = '';
+	$afzuigkapWcdIDX        = '';
+	$vaatwasserWcdIDX       = '';
+	$overrideSwitchIDX      = '';
 	
 // Ecoflow Powerstream API variables
 	$ecoflowPath		    = '/Path/2/Files/';	                 // Path waar je scripts zich bevinden
